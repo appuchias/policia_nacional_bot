a405762d839e18eacb4dfc2192f66e9eeb8648bb
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-03-10 23:32:14 +0100

    Fix bug

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.role import Role
 
 
 class Events(commands.Cog):
@@ -7,10 +8,13 @@ class Events(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        new = await self.client.fetch_role(815748222210277406)  # Truquero
+    async def on_member_join(self, member: discord.Member):
+        guild = await self.client.fetch_guild(815689898756407307)  # SrtoCarguai
         channel = await self.client.fetch_channel(815689900719734835)  # #🎊-bienvenida
-        await member.add_roles(new)
+        truquero = guild.get_role(815748222210277406)  # Truquero
+        assert type(truquero) == discord.Role
+
+        await member.add_roles(truquero)
         try:
             await member.send(f"Bienvenido al server **{member.guild}**!")
         finally:
@@ -19,7 +23,7 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         channel = await self.client.fetch_channel(818569618770886697)  # #huidas
-        await channel.send(f"{member.name}#{member.discriminator} se acaba de ir D:")
+        await channel.send(f"{member.mention} se acaba de ir D:")
 
 
 def setup(client):
