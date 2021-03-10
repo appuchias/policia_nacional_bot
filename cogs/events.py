393e92903c18c8132721aceb0438827222dc0d6c
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-03-10 23:17:15 +0100

    Update events file

--- cogs/events.py
+++ cogs/events.py
@@ -6,25 +6,20 @@ class Events(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # Events
-    # @commands.Cog.listener(name="on_message")
-    # async def on_msg(self, message):
-    #     if message.embeds or message.author == self.client.user or message.author.bot:
-    #         return
-
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        new = discord.utils.get(member.guild.roles, name="Truquero")
+        new = await self.client.fetch_role(815748222210277406)  # Truquero
+        channel = await self.client.fetch_channel(815689900719734835)  # #🎊-bienvenida
         await member.add_roles(new)
-        await member.send(f"Bienvenido al server **{member.guild}**!")
-        channel = discord.utils.get(member.guild.channels, name="🎊-bienvenida")
-        await channel.send(f"{member.mention} se acaba de unir! :tada:")
+        try:
+            await member.send(f"Bienvenido al server **{member.guild}**!")
+        finally:
+            await channel.send(f"{member.mention} se acaba de unir! :tada:")
 
     @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        user = member
-        channel = discord.utils.get(member.guild.channels, name="huidas")
-        await channel.send(f"{user} se acaba de ir, parece que no lo pasaba bien D:")
+    async def on_member_remove(self, member: discord.Member):
+        channel = await self.client.fetch_channel(818569618770886697)  # #huidas
+        await channel.send(f"{member.name}#{member.discriminator} se acaba de ir D:")
 
 
 def setup(client):
