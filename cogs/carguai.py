8a9e0ac158410200e387b2c59ed8fa87fc654a6c
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-03-11 00:13:34 +0100

    complex !di

--- cogs/carguai.py
+++ cogs/carguai.py
@@ -1,6 +1,7 @@
 from typing import Union
 import discord
 from discord.ext import commands
+import asyncio
 
 
 class Carguai(commands.Cog):
@@ -8,10 +9,31 @@ class Carguai(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def di(self, ctx, channel: Union[discord.TextChannel, int], *message: str) -> None:
+    @commands.has_permissions(administrator=True)
+    async def di(
+        self, ctx, channel: Union[discord.TextChannel, int] = 571380940664995842, *message: str
+    ) -> None:
         if type(channel) == int:
             channel = self.client.fetch_channel(channel)
-        await channel.send(message)
+        message = '"*' + " ".join(message) + '*"'
+        await ctx.send(message)
+        msg = await ctx.send("Correcto? (Si no es correcto espera un minuto)")
+        await msg.add_reaction("👍")
+
+        def check(reaction, user):
+            return (
+                reaction.message.id == msg.id
+                and user.id == ctx.message.author.id
+                and str(reaction.emoji) == "👍"
+            )
+
+        try:
+            reaction, user = await self.client.wait_for("reaction_add", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send("Tiempo agotado")
+            await msg.clear_reactions()
+        else:
+            await channel.send(message)
 
 
 def setup(client):
